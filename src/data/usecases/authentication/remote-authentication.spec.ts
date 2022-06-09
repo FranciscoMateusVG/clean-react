4c6bda9a6ccf8d9b16185bb1cd7a042a31ebf892
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-06-09 19:16:03 -0300

    refactor: changed remote auth to use faker

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,8 +1,9 @@
 import { makeSut } from './factories'
+import { faker } from '@faker-js/faker'
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpClient with correct URL', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
