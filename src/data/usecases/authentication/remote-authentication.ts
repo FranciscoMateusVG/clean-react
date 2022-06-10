04a011c1ad8344e98d66650ab48aec256a107803
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-06-09 23:18:09 -0300

    feat: ensure RemoteAuth returns AccountModel on succes

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,14 +1,16 @@
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import {
+  Authentication,
+  AuthenticationParams
+} from '@/domain/usecases/authentication'
 import {
   HttpPostClient,
-  HttpResponse,
   HttpStatusCode
 } from '@/data/protocols/http/http-post-client'
 import { InvalidCredentialsError } from '@/domain/erros/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/erros/unexpected-error'
 import { AccountModel } from '@/domain/models/account-model'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<
@@ -17,18 +19,19 @@ export class RemoteAuthentication {
     >
   ) {}
 
-  async auth(
-    params: AuthenticationParams
-  ): Promise<HttpResponse<AccountModel>> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
 
-    if (httpResponse.statusCode === HttpStatusCode.ok) return httpResponse
+    const status = httpResponse.statusCode
 
-    if (httpResponse.statusCode === HttpStatusCode.unauthorized)
-      throw new InvalidCredentialsError()
+    const { ok, unauthorized } = HttpStatusCode
+
+    if (status === ok && httpResponse.body) return httpResponse.body
+
+    if (status === unauthorized) throw new InvalidCredentialsError()
 
     throw new UnexpectedError()
   }
